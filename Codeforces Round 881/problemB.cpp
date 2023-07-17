8c9425c1db9d81c9bda531c4aacb8695bf516407
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-07-18 00:03:16 +0900

    881

--- Codeforces Round 881/problemB.cpp
+++ Codeforces Round 881/problemB.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 long long a[200005];
-
+ 
 void solve() {
     int t;
     cin >> t;
@@ -36,11 +36,11 @@ void solve() {
             }
             sum += a[i];
         }
-
+ 
         cout << sum << " " << cnt << "\n";
     }
 }
-
+ 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0); cout.tie(0);
