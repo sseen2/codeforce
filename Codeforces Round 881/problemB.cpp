9bb5d99d24be21d43b6394d357202842ab4d462b
sseen2 <59137639+sseen2@users.noreply.github.com>
2023-07-17 23:43:43 +0900

    881

--- Codeforces Round 881/problemB.cpp
+++ Codeforces Round 881/problemB.cpp
@@ -1,43 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long a[200005];
+
 void solve() {
     int t;
     cin >> t;
     while (t-- > 0) {
         long n;
         cin >> n;
-        long long a[n];
-        int num = 0, count = 1, start = 0, end = 0;
-        long long sum = 0;
-        bool first = true;
+        int cnt = 0;
+        long long sum = 0, num = 0;
+        a[n] = 1;
+        
         for (int i = 0; i < n; i++) {
             cin >> a[i];
-            if (a[i] < 0 && first) { // 시작 음수
-                first = false;
-                start = i;
-            }
-            else if (a[i] < 0) { // 끝 음수
-                end = i;
-            }
         }
-        
-        if (end == 0) {
-            end = start;
-        }
-        for (int i = start; i <= end; i++) {
-            a[i] = -a[i];
-        }
-
         for (int i = 0; i < n; i++) {
             if (a[i] < 0) {
-                count++;
-                a[i] = -a[i];
+                sum += -a[i];
+                cnt++;
+                for (int j = i + 1; j < n; j++) {
+                    if (a[j] == 0) {
+                        continue;
+                    }
+                    else if (a[j] > 0) {
+                        break;
+                    }
+                    else if (a[j] < 0) {
+                        i = j;
+                        sum += -a[j];
+                    }
+                }
+                continue;
             }
             sum += a[i];
         }
 
-        cout << sum << " " << count << "\n";
+        cout << sum << " " << cnt << "\n";
     }
 }
 
